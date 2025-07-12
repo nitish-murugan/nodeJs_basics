d4efc28bc6277e16948bbd02c35060034e1a8d8e
nitish-murugan <nitishmurugan2006@gmail.com>
2025-07-12 11:05:48 +0530

    learning basics of nodeJs

---
+++ lesson1/index.js
@@ -0,0 +1,10 @@
+console.log("Hello world");
+
+const arr = [1,2,3,4,5];
+console.log(arr);
+
+setTimeout(()=>{
+    console.log("It is executed after all the lines are executed");
+},2000);
+
+console.log("End line of the code");
