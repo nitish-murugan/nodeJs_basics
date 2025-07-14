71fea07ce013bbe836473a45050961414beca348
nitish-murugan <155057043+nitish-murugan@users.noreply.github.com>
2025-07-14 11:27:43 +0530

    lodash

--- lesson3_packageManager/index.js
+++ lesson3_packageManager/index.js
@@ -1,7 +1,7 @@
 const lodash = require('lodash');
 const arr = ["nitish", "mohan", "kumar", "karthik"];
 
-const newArr = lodash.map(arr, lodash.capitalize);
+const newArr = lodash.map(arr, lodash.capitalize); 
 console.log(newArr);
 
 
