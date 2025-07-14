3c6a371919f1c6bace5588ce963a7075639ab0ef
nitish-murugan <nitishmurugan2006@gmail.com>
2025-07-14 11:26:13 +0530

    node modules and packages

---
+++ lesson3_packageManager/index.js
@@ -0,0 +1,8 @@
+const lodash = require('lodash');
+const arr = ["nitish", "mohan", "kumar", "karthik"];
+
+const newArr = lodash.map(arr, lodash.capitalize);
+console.log(newArr);
+
+
+console.log(__dirname)
