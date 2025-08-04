73241e4922629208a6e917a6bd6ffad759bbb679
nitish-murugan <nitishmurugan2006@gmail.com>
2025-08-04 22:39:34 +0530

    basics of graphQL

---
+++ lesson15_graphql/src/data/products.js
@@ -0,0 +1,32 @@
+const products = [
+    {
+        id: "1",
+        title: "Laptop",
+        category: "Electronics",
+        price: 100.90,
+        inStock: true
+    },
+    {
+        id: "2",
+        title: "Coffee",
+        category: "Food",
+        price: 50.10,
+        inStock: true
+    },
+    {
+        id: "3",
+        title: "Speaker",
+        category: "Electronics",
+        price: 1000.90,
+        inStock: false
+    },
+    {
+        id: "4",
+        title: "Rose",
+        category: "Plant",
+        price: 200.90,
+        inStock: true
+    }
+]
+
+module.exports = products;
